a5c9227ab45b67fb97dca9f5b1db09dfd6ec7d71
Vishnu Prathap M B <vishnu.prathap@brillio.com>
2025-07-28 12:49:12 +0530

    Revereted Build Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
